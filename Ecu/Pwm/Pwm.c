7ccbcf1efd4289794f78cb560e726a091966d60d
TeamBits Embedded Systems <teambits.global@gmail.com>
2025-12-09 17:48:33 +0530

    Added global variables to Pwm driver

--- Ecu/Pwm/Pwm.c
+++ Ecu/Pwm/Pwm.c
@@ -1,44 +1,42 @@
 #include "Pwm.h"
 
-void Square_Wave_Set(int ton)
+uint32_t pwm_freq =0; // in Hz
+uint32_t pwm_duty =0; // in %
+uint32_t pwm_volt =0; // in mV
+
+
+void Pwm_Set(void)
 {
-	//Set the Gpt base mode as Internal timer to make use of internal clock.
-	Gpt1_Base_Mode_Set(INTERNAL_TIMER);
+    // Use global variables pwm_freq (Hz) and pwm_duty (%)
+    // Calculate period and on-time
+    uint32_t period = 0;
+    uint32_t ton = 0;
+    if (pwm_freq > 0) {
+	 period = 1000000 / pwm_freq; // period in microseconds
+	 ton = (period * pwm_duty) / 100; // on-time in microseconds
+    }
 
-	// Set the match0 value (for 5ms Ton , match value = 5ms/1us = 5000)
-	Gpt1_Match0_Count_Set(ton);
+    // Calculate effective voltage (0 to 3300 mV)
+    pwm_volt = (3300 * pwm_duty) / 100;
 
-	// Set the counter initial value to 0
-	Gpt1_Count_Set(0);
+    // Set the Gpt base mode as Internal timer to make use of internal clock.
+    Gpt1_Base_Mode_Set(INTERNAL_TIMER);
 
-	// Set the match0 internal option: counter reset
-	Gpt1_Match0_iOption_Set(COUNTER_RESET);
+    // Set the match1 value (determines Pwm time period)
+    Gpt1_Match1_Count_Set(period);
 
-	// Set the match0 external option: toggle the pin
-	Gpt1_Match0_eOption_Set(MAT_PIN_TOGGLE);
-}
+    // Set the match0 value (determines Pwm on-time)
+    Gpt1_Match0_Count_Set(ton);
 
-void Pwm_Wave_Set(int period, int ton)
-{
-//	Set the Gpt base mode as Internal timer to make use of internal clock.
-	Gpt1_Base_Mode_Set(INTERNAL_TIMER);
+    // Set the counter initial value to 0
+    Gpt1_Count_Set(0);
 
-//	Set the match1 value (determines Pwm time period)
-	Gpt1_Match1_Count_Set(period);
+    // Set the match0 internal option: counter continue
+    Gpt1_Match0_iOption_Set(COUNTER_CONTINUE);
 
-//	Set the match0 value (determines Pwm on-time)
-	Gpt1_Match0_Count_Set(ton);
+    // Set the match1 internal option: counter reset
+    Gpt1_Match1_iOption_Set(COUNTER_RESET);
 
-//	Set the counter initial value to 0
-	Gpt1_Count_Set(0);
-
-//	Set the match0 internal option: counter continue
-	Gpt1_Match0_iOption_Set(COUNTER_CONTINUE);
-	
-//	Set the match1 internal option: counter reset
-	Gpt1_Match1_iOption_Set(COUNTER_RESET);
-
-//	Set the match0 external option: enable Pwm
-	Gpt1_Match0_eOption_Set(MAT_PIN_PWM);
-
+    // Set the match0 external option: enable Pwm
+    Gpt1_Match0_eOption_Set(MAT_PIN_PWM);
 }
--- Ecu/Pwm/Pwm.h
+++ Ecu/Pwm/Pwm.h
@@ -1,12 +1,16 @@
 #ifndef __PWM_H__
 #define __PWM_H__
 
+#include <stdint.h>
 #include "Gpt.h"
 
-void Square_Wave_Set(int ton);
-void Pwm_Wave_Set(int period, int ton);
+extern uint32_t pwm_freq; // in Hz
+extern uint32_t pwm_duty; // in %
+extern uint32_t pwm_volts // in mv
 
 
+
+void Pwm_Set(void);
 
 
 #endif
